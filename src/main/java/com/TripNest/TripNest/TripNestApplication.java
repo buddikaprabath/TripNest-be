3cd5d5a52c68e34e0b6e5e1589124245673283aa
buddikaprabath <hmbuddikaprabath@gmail.com>
2024-06-07 14:37:03 +0530

    init commit

---
+++ src/main/java/com/TripNest/TripNest/TripNestApplication.java
@@ -0,0 +1,13 @@
+package com.TripNest.TripNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripNestApplication.class, args);
+	}
+
+}
